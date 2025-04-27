a5314f7b2c4c79330ac46942a4a81287059503f8
Ajay <ajayjadhav0913@gmail.com>
2025-04-27 16:34:23 +0530

    Rate-limit added

---
+++ Rate-limit.js
@@ -0,0 +1,30 @@
+const express =  require('express')
+const app = express()
+const ratelimit = require('express-rate-limit');
+const { STATUS_CODES } = require('http');
+app.use(express.json());
+const port = 8000
+
+const  limiter = ratelimit(
+    {
+        
+       windowMs:60*60*1000,
+       max:5,
+       STATUS_CODES:429,
+       message:"to many request from same  IP address"
+    }
+)
+app.use(limiter)
+
+app.get('/', (req, res)=>{
+    res.send("hello server i am ajay....");
+});
+app.get('/about',(req, res)=>{
+    res.send("this is the  about section ");
+})
+
+
+
+app.listen(port, ()=>{
+    console.log("server is running ...");
+} )
